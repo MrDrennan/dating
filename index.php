0098d533daf4ea616a68df81a4f51c7288d5ecaa
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-01-24 02:28:41 -0500

    refactored form tag html placement

--- index.php
+++ index.php
@@ -18,4 +18,9 @@ $f3->route('GET /', function() {
    echo $view->render('views/home.html');
 });
 
+$f3->route('GET /create-profile/personal-info', function() {
+   $view = new Template();
+   echo $view->render('views/frm-personal-info.html');
+});
+
 $f3->run();
